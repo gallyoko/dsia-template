f6699cc678a7ed483149a8a94716294831a1dd73
gally <frederic.penot@gmail.com>
2023-08-11 17:46:44 +0200

    Delete src directory

--- src/template/files/templates/src/lib/components/__componentName@dasherize__/model/__secondModelName@dasherize__.model.ts
+++
@@ -1,7 +0,0 @@
-export class <%= classify(secondModelName) %>Light {
-    <%= camelize(secondModelKey) %>: string;
-}
-
-export class <%= classify(secondModelName) %> extends <%= classify(secondModelName) %>Light{
-    
-}
--- src/template/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Rule } from '@angular-devkit/schematics';
-import { Schema } from './schema';
-export declare function dsiaTemplate(_options: Schema): Rule;
-export declare function tolowercase(value: string): string;
-export declare function touppercase(value: string): string;
